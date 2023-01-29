c6bbf67aa511158efb9f6fe302626a7eea190765
gharjare <gharjarechetan@gmail.com>
2023-01-29 15:46:11 +0530

    [chetan gharjare] check lines are greater or smaller or equal

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@
             Console.WriteLine("Welcome to line comparison problem");
             int x1 = 4, x2 = 5, y1 = 6, y2 = 7;
             int x3 = 8, x4 = 5, y3 = 5, y4 = 7;
+            Console.WriteLine(1.CompareTo(1));
+            Console.WriteLine(1.CompareTo(2));
+            Console.WriteLine(2.CompareTo(1));
             Console.WriteLine("The points x1 = " + x1);
             Console.WriteLine("The points x2 = " + x2);
             Console.WriteLine("The points y1 = " + y1);
@@ -19,13 +22,18 @@
             Console.Write("Lenght of a Line is : {0}", lengthOfLine1);
             double lengthOfLine2 = Math.Sqrt(Math.Pow((x4 - x3), 2) + Math.Pow((y4 - y3), 2));
             Console.WriteLine("Lenght of Second Line is : {0}", lengthOfLine2);
-            if(lengthOfLine1 == lengthOfLine2)
+            int length = lengthOfLine1.CompareTo(lengthOfLine2);
+            if (length > 0)
             {
-                Console.WriteLine("line one and line two are equal");
+                Console.WriteLine("Line 1 is greater than line 2");
+            }
+            else if (length < 0)
+            {
+                Console.WriteLine("line 1 is smalller than line 2");
             }
             else
             {
-                Console.WriteLine("line 1 and line 2 not equal");
+                Console.WriteLine("line 1 and line 2 are equal");
             }
         }
     }
