e824279ec77a6b8834118ca9320a523559eedbc5
gharjare <gharjarechetan@gmail.com>
2023-01-29 15:21:50 +0530

    [chetan gharjare] Crete one line and calulate lenth of line

--- Program.cs
+++ Program.cs
@@ -5,7 +5,13 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to line comparison problem");
-
+            int x1 = 4, x2 = 5, y1 = 6, y2 = 7;
+            Console.WriteLine("The points x1 = " + x1);
+            Console.WriteLine("The points x2 = " + x2);
+            Console.WriteLine("The points y1 = " + y1);
+            Console.WriteLine("The points y2 = " + y2);
+            double lengthOfLine1 = Math.Sqrt(((x2 - x1) ^ 2) + ((y2 - y1) ^ 2));
+            Console.Write("Lenght of a Line is : {0}", lengthOfLine1);
         }
     }
 }
